08b635e5bfd61433e6d553859479f072861e1e22
Finlay Maguire <finlaymaguire@gmail.com>
2014-04-06 02:48:13 +0100

    more i3 dotfiles

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 mv ~/.vimrc ~/.vimrc_backup
 mv ~/.i3status.conf ~/.i3status.conf_backup
+mv ~/.i3 ~/.i3_backup
 mv ~/.zshrc ~/.zshrc_backup
 mv ~/.zsh ~/.zsh_backup
 mv ~/.Xresources ~/.Xresources_backup
@@ -9,6 +10,7 @@ mv ~/.irssi ~/.irssi_backup
 
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/i3status.conf ~/.i3status.conf
+ln -s $PWD/i3 ~/.i3
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/zsh ~/.zsh
 ln -s $PWD/Xresources ~/.Xresources
